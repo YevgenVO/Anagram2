38dbda2f4b2adcb1c50b3dfcbb1e7cc6f72f9b42
Yevgen Ostapiuk <yevgen.v.o@gmail.com>
2016-10-27 13:35:04 +0300

    fifth attempt

--- Anagram2/Anagram.java
+++ Anagram2/Anagram.java
@@ -22,19 +22,19 @@ public class Anagram {
             if (line.charAt(i) == SPACE && i > 0) {
                 firstLetter = lastLetter + 2;
                 lastLetter = i - 1;
-                addAnagram(firstLetter, lastLetter);
+                creatAnagram(firstLetter, lastLetter);
                 line = line + " ";
             }
             if (i == length - 1) {
                 firstLetter = lastLetter + 2;
                 lastLetter = length - 1;
-                addAnagram(firstLetter, lastLetter);
+                creatAnagram(firstLetter, lastLetter);
             }
         }
         line = line.substring(length);
     }
 
-    private static void addAnagram(int firstLetter, int lastLetter) {
+    private static void creatAnagram(int firstLetter, int lastLetter) {
         for (int i = 0; i < lastLetter - firstLetter + 1; i++) {
             line = line + line.charAt(lastLetter - i);
         }
